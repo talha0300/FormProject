9a8953fdb5faed1e16fd317b89ec2b6de88367d0
“talha0300” <talhaali0300@gmail.com>
2020-08-27 10:41:41 +0500

    chamges

--- app/js/form.js
+++ app/js/form.js
@@ -1,12 +1,18 @@
 
-$('input,textarea').on('input',function(){
-  var inpObj1 = document.getElementById("name").checkValidity();
-  var inpObj2 = document.getElementById("email").checkValidity();
-  var inpObj3 = document.getElementById("age").checkValidity();
-  var inpObj4 = document.getElementById("dropdown").checkValidity();
-  var inpObj5 = document.getElementById("most-like").checkValidity();
-  var inpObj7 = document.getElementById("comment").checkValidity();
-  console.log(inpObj7);
+$('.form-input,.input-textarea,.input-checkbox,.input-radio').on('input',function(){
+  var name = $('#name')[0].checkValidity();
+  var email = $('#email')[0].checkValidity();
+  var age = $('#age')[0].checkValidity();
+  var role = $('#dropdown')[0].checkValidity();
+  var most_like = $('#most-like')[0].checkValidity();
+  var coment = $('#comment')[0].checkValidity();
+  //var inpObj1 = document.getElementById("name").checkValidity();
+  //var inpObj2 = document.getElementById("email").checkValidity();
+  //var inpObj3 = document.getElementById("age").checkValidity();
+  //var inpObj4 = document.getElementById("dropdown").checkValidity();
+  //var inpObj5 = document.getElementById("most-like").checkValidity();
+  //var inpObj7 = document.getElementById("comment").checkValidity();
+  //console.log(inpObj7);
   var requiredCheckboxes = $('input[type="checkbox"]');
   //console.log('6');
   //requiredCheckboxes.change(function(){
@@ -15,7 +21,7 @@ $('input,textarea').on('input',function(){
             requiredCheckboxes.removeAttr('required');
             //requiredCheckboxes.removeAttr('oninvalid');
             //requiredCheckboxes.removeAttr('oninput');
-            inpObj6=true
+            checked=true
             //onsole.log('4');
         }
 
@@ -24,19 +30,19 @@ $('input,textarea').on('input',function(){
             //requiredCheckboxes.attr('oninvalid','Invalid entry');
             //requiredCheckboxes.attr('oninput','');
             //console.log('5');
-            inpObj6=false
+            checked=false
         }
 
-    console.log(inpObj1,inpObj2,inpObj3,inpObj4,inpObj5,inpObj6,inpObj7);
-    if(inpObj1 && inpObj2 && inpObj3 && inpObj4 && inpObj5 && inpObj6 && inpObj7)
+    //console.log(name,email,age,role,most_like,coment,checked);
+    if(name && email && age && role && most_like && coment && checked)
     {
-      $('#submit').css('background','#37af65')
-      $('#submit').css('color','#f3f3f3')
-      console.log('76');
+      $('.submit-button').css('background','#37af65')
+      $('.submit-button').css('color','#f3f3f3')
+      //console.log('76');
     }
     else {
-      $('#submit').css('background','#f3f3f3')
-      $('#submit').css('color','red')
+      $('.submit-button').css('background','#f3f3f3')
+      $('.submit-button').css('color','red')
     }
   //var inpObj = document.getElementById("name").checkValidity();
   //console.log(this.checkValidity());
@@ -55,9 +61,9 @@ $('input,textarea').on('input',function(){
 });
 
 $('#reset').on('click',function(){
-  $('#submit').css('background','#f3f3f3')
-  $('#submit').css('color','red')
-  console.log('3');
+  $('.submit-button').css('background','#f3f3f3')
+  $('.submit-button').css('color','red')
+  //console.log('3');
 });
 
 
@@ -66,21 +72,21 @@ $('#reset').on('click',function(){
 $(document).ready(function () {
     $('#submit').on('click',function() {
       var requiredCheckboxes = $(':checkbox[required]');
-      console.log('6');
+      //console.log('6');
         requiredCheckboxes.change(function(){
             //console.log("message")
             if(requiredCheckboxes.is(':checked')) {
                 requiredCheckboxes.removeAttr('required');
                 requiredCheckboxes.removeAttr('oninvalid');
                 requiredCheckboxes.removeAttr('oninput');
-                console.log('4');
+                //console.log('4');
             }
 
             else {
                 requiredCheckboxes.attr('required', 'required');
                 requiredCheckboxes.attr('oninvalid','Invalid entry');
                 requiredCheckboxes.attr('oninput','');
-                console.log('5');
+                //console.log('5');
             }
         });
     });
